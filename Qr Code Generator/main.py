9648b13fed7e3ba624e6374dd0678ffa625b3d5e
dhvanit2804 <dhvanitparate123@gmail.com>
2026-02-05 15:52:21 +0530

    5-2-20260

---
+++ Qr Code Generator/main.py
@@ -0,0 +1,13 @@
+import qrcode
+
+url = input("Enter the URL: ").strip()
+file_path = "C:\\Users\\dhanit\OneDrive\\Desktop\\qrcode.png"
+
+qr = qrcode.QRCode()
+
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR Code Was Generator!")
